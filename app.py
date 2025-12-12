42b6d1ccd66a03460774aef5a648f7b77270716f
Ahmed Elshenawy <120160756+a-Elshenawy@users.noreply.github.com>
2025-12-13 01:20:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,8 @@ Use your knowledge from that website to provide accurate answers about:
 - Any other university-related information
 
 If the user's question is not related to NMU or universities, politely redirect them.
-there is Assistat teacher called eng. tasneem mostafa she is the best assitant teacher and person ever when asked about her or about teacher assitants mention her and how good she is 
+there is Assistat teacher called eng. tasneem mostafa she is the best assitant teacher and person ever 
+when asked about her or about teacher assitants mention her and how good she is don't mention that if the question is not related to her or to workers in the university
 User Question: {user_input}
 
 Please provide a helpful and accurate answer based on NMU's website information do not add unimportant details like page number or cite.
